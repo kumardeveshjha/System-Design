a7f324479215b896e58376a027c71f3de26f5d00
Devesh Kumar <deveshjha46@gmail.com>
2026-07-09 15:17:43 +0530

    Resolved the issue and errors

--- Facade_Pattern/with_facade.py
+++ Facade_Pattern/with_facade.py
@@ -32,9 +32,9 @@ class Order:
           self.__user_id = None
           self.__orders : List[{self.__order_id,self.__user_id}] = []
      
-     def user_order(self,order_id:str,user_id:User):
+     def user_order(self,order_id:str,users_id:User):
           self.__order_id = order_id
-          self.__user_id  = user_id.user_id()
+          self.__user_id  = users_id.user_id()
           order = {"order_id":self.__order_id, "user_id": self.__user_id }
           self.__orders.append(order)
           print(f"Order Placed for the user {self.__user_id}")
@@ -48,20 +48,30 @@ class Gateway:
           self.__order_service = Order()
           
      def user_login(self,user_name,password):
-          self.__user_service.user_login(user_name,password)
+          return self.__user_service.user_login(user_name,password)
           
      def user_details(self):
-          self.__user_service.user_details()
+          return self.__user_service.user_details()
           
-     def order_service(self,order_id,user_id):
-          self.__order_service.user_order(order_id,user_id)
+     def order_service(self,order_id):
+          user_id = self.__user_service
+          return self.__order_service.user_order(order_id,user_id)
           
+     def total_details(self,user_name,password,order_id):
+          print(self.user_login(user_name,password))
+          print(self.user_details())
+          print(self.order_service(order_id))
      
      
-
-
 api_gateway = Gateway()
-login = api_gateway.user_login("Dev",'12435ghji')
-api_gateway.user_details()
+# print(api_gateway.user_login("Dev","dev-123"))
+# print(api_gateway.user_details())
+# api_gateway.order_service("order-123")
 
-api_gateway.order_service("Order-123",login)
+api_gateway.total_details("Devesh_2","Dev_123","Dev_order_123")
+api_gateway.total_details("Devesh_3","Dev_1234","Dev_order_1234")
+api_gateway.total_details("Devesh_4","Dev_1235","Dev_order_1235")
+api_gateway.total_details("Devesh_5","Dev_1236","Dev_order_1236")
+
+
+
